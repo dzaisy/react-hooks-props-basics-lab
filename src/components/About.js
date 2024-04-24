dd4c8806208c14e333c88b1578d8e7ac72427ca0
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-04-24 12:38:48 +0300

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  let content = null
+  if (props.bio && props.bio !== '') {
+    content = <p>{props.bio}</p>
+  };
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {content}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,20 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        name={user.name}
+        city={user.city}
+        color={user.color}
+      />
+      <About 
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
+
+
 
 export default App;
